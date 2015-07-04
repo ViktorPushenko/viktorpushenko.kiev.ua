c347d57da5228045eefce9c61d7239edff94b6ef
Viktor Pushenko <viktorpushenko@ukr.net>
2015-07-04 13:19:02 +0300

    Tooltips done

---
+++ app/js/contacts.js
@@ -0,0 +1,60 @@
+// Модуль связаться со мной 
+var contactMe = (function (){
+
+	var init = function(){
+				console.log('Инициализация модуля contacts');
+				setUpListners();
+			},
+
+		setUpListners = function (){
+			$('#form').on('submit', submitForm); // отправка формы "связаться со мной"
+			},
+		submitForm = function (ev) {
+	      	console.log('Работаем с формой связи');
+
+	      	ev.preventDefault();
+
+	      	var form = $(this),          
+	          	url = '/send_mail.php',
+	          	defObject = ajaxForm(form, url);
+
+	      	if (defObject) {
+	        	
+	        	defObject.done(function(ans) {
+	          	
+	          	var mes = ans.mes,
+	            	status = ans.status;
+
+		        if ( status === 'OK'){
+		            form.trigger('reset');
+		            form.find('.success-mes').text(mes).show();           
+		        } else{
+		            form.find('.error-mes').text(mes).show();
+		        }
+	        });
+	      }
+	    },
+
+	    ajaxForm = function (form, url) {
+      
+	      	if (!validation.validateForm(form)) return false;  // Возвращает false, если не проходит валидацию 
+	    	var data = form.serialize(); // собираем данные из формы в объект data
+
+	      	return $.ajax({ // Возвращает Deferred Object
+		        type: 'POST',
+		        url: url,
+		        dataType : 'JSON',
+		        data: data })
+	      		.fail( function(ans) {
+			        console.log('Проблемы в PHP');
+			        form.find('.error-mes').text('На сервере произошла ошибка').show();
+		      	});
+	    };   
+
+	return {
+		init: init
+	};
+
+})();
+
+contactMe.init();
---
+++ app/js/validation.js
@@ -0,0 +1,110 @@
+// Модуль валидации
+var validation = (function (){
+
+	// ------------------------------------- PUBLIC FUNCTIONS -------------------------------------
+	var
+	
+	init = function() {
+		console.log('Инициализация модуля validation');
+		_setUpListners();
+	},
+
+	validateForm = function(form) {
+		console.log("validation ");
+
+		form.preventDefault();
+
+		var
+		elements 	= $('form').find('input, textarea').not('input[type="file"], input[type="hidden"]'),
+		valid 		= true;
+
+		$.each(elements, function(index, val) {
+	        var element = $(val),
+	            val = element.val(),
+	            pos = element.attr('qtip-position');         
+
+	        if(val.length === 0){
+	          element.addClass('has-error');
+	          _createTooltip(element, pos);            
+	          valid = false;
+	        }
+	    });
+
+	    return valid;
+	};
+	
+	// ------------------------------------- PRIVATE FUNCTIONS -------------------------------------
+	var 
+	_setUpListners = function() {
+		console.log('Установка слушателей');
+		$('form').	on('submit', validateForm).
+					on('keydown', '.has-error', _removeError).
+					on('reset', _clearForm);
+	},
+	
+	_removeError = function(form) {
+		console.log("removing errors");
+		$(this).removeClass('has-error');
+	},
+
+	_clearForm = function(form) { // Очищает форму 
+	      console.log('Очищаем форму');
+
+	      var form = $(this);
+	      form.find('.input, .textarea').trigger('hideTooltip'); // удаляем тултипы
+	      form.find('.has-error').removeClass('has-error'); // удаляем красную подсветку
+	      form.find('.error-message, success-message').text('').hide(); // очищаем и прячем сообщения с сервера
+	    }
+
+	_createTooltip = function (element, position) { // Создаёт тултипы
+	      console.log('Создаем тултип');
+
+	      // позиция тултипа
+	      if (position === 'right') {
+	        position = {
+	          my: 'left center', 
+	          at: 'right center'
+	        }
+	      }else{
+	        position = {
+	          my: 'right center', 
+	          at: 'left center',
+	          adjust: {
+	            method: 'shift none'
+	          }
+	        }
+	      }
+
+	      // инициализация тултипа
+	      element.qtip({
+	        content: {
+	          text: function() {
+	            return $(this).attr('qtip-content');
+	          }
+	        },
+	        show: {
+	          event: 'show'
+	        },
+	        hide: {
+	          event: 'keydown hideTooltip'
+	        },
+	        position: position,
+	        style: {
+	          classes: 'qtip-mystyle qtip-rounded',
+	          tip: {
+	            height: 10,
+	            width: 16
+	          }
+	        }
+	      }).trigger('show');
+    	};
+
+	//  ------------------------------------- FINISH -------------------------------------
+	return {
+		init: init,
+		validateForm: validateForm
+	};
+
+})();
+
+validation.init();
